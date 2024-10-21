0e9860c159eacc4ee37dc7afdb5be8186bb7e972
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-21 15:00:22 +0200

    Update 4-puts.c file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* Print message on screen */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\"");
 	puts("");
 	return (0);
 }
