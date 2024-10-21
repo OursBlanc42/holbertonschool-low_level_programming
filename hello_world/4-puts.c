1852b1a2d38082c014375c9b6fe9130950fb785e
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-21 15:10:00 +0200

    Update 4-puts.c file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Display sentence on screen'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-/* The below code will display sentence on screen */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
