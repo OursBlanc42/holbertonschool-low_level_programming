28935fafd255c6e21846ea434b2807d3a487f90e
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-23 16:35:13 +0200

    Update file 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: Display alphabet 10 times on screen without printf or putchar
+ * _putchar: Emulate function putchar
+ * Return: Display alphabet
+ */
+
+void print_alphabet(void)
+{
+	char *str = "abcdefghijklmnopqrstuvwxyz"; /* chain to display */
+
+	int a = 0; /* loop counter */
+
+	/* while loop to display alphabet 10 times*/
+	while (a < 10)
+	{
+		/* reset pointer */
+		char *ttr = str; 
+
+		while (*ttr)
+		{
+			_putchar(*ttr++);
+		}
+		_putchar('\n');
+
+	/* increment counter */
+	a++;
+
+	}
+}
