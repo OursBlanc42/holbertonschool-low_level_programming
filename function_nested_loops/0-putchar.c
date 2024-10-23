051ad5c654ec74edb043af47cdad67e34e906912
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-23 10:00:36 +0200

    add 0-putchar.c file

---
+++ function_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Display "_putchar" on screen
+ * Return: '_putchar'
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
