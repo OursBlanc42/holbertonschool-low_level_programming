e57ae5dc13cb93b7284e0e39201a9f634c8fbf19
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-25 20:06:56 +0200

    Add 9-times_table.c & update main.h

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * Description:  Prints 9 times tables
+ * Return: always 0 success
+ */
+
+void times_table(void)
+{
+	/* declare variable */
+	int i, j, n, d, u;
+
+	/* start table */
+	for (j = 0 ; j <= 9 ; j++)
+	{
+
+		for (i = 0 ; i <= 9 ; i++)
+		{
+			n = i * j;
+			d = n / 10;
+			u = n % 10;
+
+			if (i == 0)
+			{
+				_putchar('0');
+			}
+			else if (i == 9)
+			{
+				if (d != 0)
+				{
+					_putchar(',');
+					_putchar('.');
+					_putchar(d + '0');
+					_putchar(u + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar('.');
+					_putchar('.');
+					_putchar(u + '0');
+				}
+			}
+			else
+			{
+				if (d != 0)
+				{
+					_putchar(',');
+					_putchar('.');
+					_putchar(d + '0');
+					_putchar(u + '0');
+
+				}
+				else
+				{
+					_putchar(',');
+					_putchar('.');
+					_putchar('.');
+					_putchar(u + '0');
+
+				}
+			}
+
+		}
+
+		/* break line and star another table*/
+		_putchar('\n');
+
+	}
+}
