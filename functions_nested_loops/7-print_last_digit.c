a0261b7964888f120683646d8eec0e0dce3e7b53
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-26 10:39:21 +0200

    Update 7-print_last_digit.c for negative case

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,6 +13,12 @@ int print_last_digit(int n)
 	/* declare variable */
 	int last_digit;
 
+	/* conver to abs value */
+	if (n < 0)
+	{
+	n = n * -1;
+	}
+
 	/* with modulo extract last digit */
 	last_digit = n % 10;
 
