81aef6af92f31422f859e32557ef768d2695e6e2
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-28 15:52:57 +0100

    Renmmad directory (wrong name)

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* 
+ * File: main.h
+ * Description: Header file that contains prototypes functions.
+ * This header ensures that the functions can be used in other files by
+ * declaring it here, so other source files can include this header and call it.
+ */
+
+/**
+ * _putchar - writes the character c to stdout
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - akes a pointer to an int as parameter and updates the value it points to to 98.
+ */
+void reset_to_98(int *n);
+
+
+#endif
+
