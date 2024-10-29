172bf0e2ae52fd88dcf52db6044b0dfa94b1d6bb
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-29 10:56:16 +0100

    Add 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,6 +13,7 @@ void puts_half(char *str)
 	/* declare and initialize variable */
 	int end = 0;
 	int start;
+	int n;
 
 	/* loop through chain until null charactere ('\0') */
 	while (str[end] != '\0')
@@ -24,8 +25,12 @@ void puts_half(char *str)
 	/* adjust end */
 	end = end - 1;
 
-	/* adjust start */
-	start = (end + 1) / 2;
+	/* define n */
+	n = (end - 1) / 2;
+
+	/* define start */
+	start = end - n;
+
 
 	/* loop through chain and display */
 	for (; start <= end ; start++)
