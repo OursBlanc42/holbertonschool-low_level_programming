87d4e9cd92dfa534db2bdbb54c566c7e4e8312c9
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-29 17:44:45 +0100

    Add 100-atoi.c and update main.h

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,69 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ * Description: convert string to an integer
+ * @s : entry string
+ * Return: integer
+ */
+
+
+int _atoi(char *s)
+{
+	/* declare and define variable */
+	int value = 0;
+	int count = 0;
+	int minus = 0;
+	int first_digit = 0;
+	int last_digit = 0;
+	int i = 0;
+	int j = 0;
+	int power = 1;
+
+	/* loop through source chain until null charactere ('\0') */
+	while (s[count] != '\0')
+	{
+		/* count the occurence of minus to detect if its negative*/
+		if(s[count] == '-')
+		{
+			minus++;
+		}
+		/* check there is a digit*/
+		if (s[count] >= '0' && s[count] <= '9')
+		{
+			/* setup variable */
+			first_digit = count;
+			last_digit = count;
+			break;
+		}
+		/* increment count for each loop */
+		count++;
+	}
+
+	/* as soon as the first digit is found, loop to the last digit found */
+	while (s[count] >= '0' && s[count] <= '9')
+	{
+		last_digit++;
+		count++;
+	}
+
+	/* from last to first digit 
+	recalculate each power of 10 at each rank to recompose the number*/
+	for (i = last_digit - 1 ; i >= first_digit ; i--)
+	{
+		j = (s[i] - '0') * power;
+		value = value + j;
+		power *= 10;
+	}
+
+		/* determinate if its negative */
+	if (minus % 2 == 1)
+	{
+		value = value * -1;
+	}
+
+	return (value);
+
+}
+
