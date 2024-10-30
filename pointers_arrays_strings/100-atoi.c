2a96497439ef5cfc8b7a29222d9210527a05803c
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-30 16:00:54 +0100

    betty compliant

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,30 +2,76 @@
 #include "main.h"
 
 /**
- * _atoi - Entry point
- * Description: convert string to an integer
- * @s : entry string
+ * recompose_value - recompose value
+ * Description: recalculate powers of 10 for each digit
+ * to recompose the number
+ * @s: string to check
+ * @first_digit: first digit
+ * @last_digit: last digit
+ * Return: recomposed value
+ */
+unsigned int recompose_value(char *s, int first_digit, int last_digit)
+{
+	unsigned int value = 0;
+	int power = 1;
+	int i;
+
+	for (i = last_digit - 1; i >= first_digit; i--)
+	{
+		int digit_value = (s[i] - '0') * power;
+
+		value += digit_value;
+
+		/* Avoid overflow by limiting power to 1,000,000,000 */
+		if (power != 1000000000)
+		{
+			power *= 10;
+		}
+	}
+
+	return (value);
+}
+
+/**
+ * result_calculation - display the result
+ * Description: convert value to result (convert to signed)
+ * @value : entry
+ * @minus : check the sign
  * Return: integer
  */
+int result_calculation(unsigned int value, int minus)
+{
+	int result = 0;
 
+	if (minus % 2 == 1)
+	{
+		result = value * -1;
+	}
+	else
+	{
+		result = value;
+	}
+	return (result);
+}
+
+/**
+ * _atoi - Entry point
+ * Description: convert string to an integer
+ * @s: entry string
+ * Return: integer
+ */
 int _atoi(char *s)
 {
 	/* declare and define variable */
 	unsigned int value = 0;
-	int count = 0;
-	int minus = 0;
-	int first_digit = 0;
-	int last_digit = 0;
-	int i = 0;
-	int j = 0;
-	int power = 1;
-	int result =0;
+	int result = 0, count = 0, minus = 0;
+	int first_digit = 0, last_digit = 0;
 
 	/* loop through source chain until null charactere ('\0') */
 	while (s[count] != '\0')
 	{
 		/* count the occurence of minus to detect if its negative*/
-		if(s[count] == '-')
+		if (s[count] == '-')
 		{
 			minus++;
 		}
@@ -48,33 +94,12 @@ int _atoi(char *s)
 		count++;
 	}
 
-	/* from last to first digit 
-	recalculate each power of 10 at each rank to recompose the number*/
-	for (i = last_digit - 1 ; i >= first_digit ; i--)
-	{
-		j = (s[i] - '0') * power;
-		value = value + j;
+	/* call function */
+	value = recompose_value(s, first_digit, last_digit);
 
-		/* avoid problem with INT MAX or INT MIN */
-		if (power != 1000000000)
-		{
-		power *= 10;
-		}
-	}
-
-	/* determinate if its negative */
-	if (minus % 2 == 1)
-	{
-		result = value * -1;
-	}
-	else
-	{
-		result = value;
-	}
-
-
-
+	/* call function */
+	result = result_calculation(value, minus);
 	return (result);
-
 }
+
 
