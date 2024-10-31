c142a3b4487b1633432b57ed1463f97b987d8955
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-10-31 17:05:11 +0100

    101-keygen.c added

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: generate right password for 101-crackme
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char *str = "zzzzzzzzzzzzzzzzzzzzzzX";
+
+	printf("%s\n", str);
+
+	return (0);
+}
