7d1c2e8820bc505a0d1a522ee1b13c5402e36f33
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-11-06 10:57:57 +0100

    3-mul.c added

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: multiplies two numbers (result as an integer)
+ * @argc: number of arguments
+ * @argv: array who contains each arguments
+ * Return: 0 if success or 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	/* declare and initialize variable */
+	int result = 0;
+	int a = 0;
+	int b = 0;
+	/* hide argc and argv to avoid compilator error*/
+	(void)argc;
+	(void)argv;
+
+	/* Check if we have 2 arguments (= 2 numbers)*/
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	/* Multiply and display result */
+	else
+	{
+		/* Convert char to int */
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		result = (a * b);
+		printf("%d\n", result);
+		return (0);
+	}
+}
