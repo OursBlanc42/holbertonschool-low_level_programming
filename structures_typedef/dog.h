88dfd9996f1a7e65e6f8f46a419b4283d9727d04
OursBlanc <regniersimon@hotmail.com>
2024-11-13 10:01:45 +0100

    modify comments in dog.h file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * File: dog.h
- * Description: Header file that contains prototypes functions and structures
- * This header ensures that the functions and structures
- * can be used in other files by declaring it here, so other source files
- * can include this header and call it.
- */
-
 /**
  * struct dog - About dog informations
  * @name: doggo name
@@ -23,10 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typedef for the type struct dog
- */
-typedef struct dog dog_t;
 
 #endif
