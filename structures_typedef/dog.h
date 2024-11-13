c278306d6d3e715127e62f5767a22ad985a03d58
OursBlanc <regniersimon@hotmail.com>
2024-11-13 10:19:17 +0100

    add ; at the end of prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -34,6 +34,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
  * @d: pointer to the structure dog
  * Return:	/
  */
-void print_dog(struct dog *d)
+void print_dog(struct dog *d);
 
 #endif /* _DOG_H_ */
