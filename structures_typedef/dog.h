d0f9e747be5eb39cd7e2cbc07a899e874691f2e9
OursBlanc <regniersimon@hotmail.com>
2024-11-13 10:16:23 +0100

    update 1-init dog and dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,4 +16,16 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - initialize dog structure
+ * Description : initialize dog structure
+ * @d: pointer to the structure dog
+ * @name: doggo name
+ * @age: doggo age
+ * @owner: doggo owner
+ *
+ * Return:	/
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* _DOG_H_ */
