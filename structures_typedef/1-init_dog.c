fecd1571a421bf525941780e471cbf85e2be1f01
OursBlanc <regniersimon@hotmail.com>
2024-11-13 10:11:40 +0100

    add 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -13,7 +13,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	/* check if d is a valid pointer */
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
