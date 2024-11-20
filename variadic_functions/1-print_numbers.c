1a1cf7dc40a8fd6bcbd9129e7056ff7d8732e489
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-11-20 13:37:30 +0100

    check special case

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -14,36 +14,40 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_list args; /* Arguments list declaration */
-
 	va_start(args, n); /* Initialize list with fixed arguments */
 
-	/* check special case */
-	if (separator == NULL)
-	{	/* if separator = NULL replace by nothing to dont print it */
-		separator = "";
-	}
-
 	if (n <= 0)
-	{	/* if n <= 0 just print the separator (or nothing as checked before) */
-		printf("%s", separator);
-		return;
+	{	/* if n <= 0 print separtor or nothing*/
+		if (separator == NULL)
+		{	/* if separator = NULL we dont print it */
+			return; /* so just return void */
+		}
+		else
+		{	/* if not print the separator */
+			printf("%s", separator);
+			return;
+		}
 	}
 
 	/* loop each arguments start with "n" */
 	/* (n-1) to stop before the last and not print the last separator */
 	for (i = 0; i < (n - 1); i++)
 	{	/* catch each args as a int */
-		printf("%d%s", va_arg(args, int), separator);
+		if (separator == NULL)
+		{	/* if separator = NULL we dont print it */
+			printf("%d", va_arg(args, int));
+		}
+		else
+		{	/* else we print number AND separator */
+			printf("%d%s", va_arg(args, int), separator);
+		}
 	}
 
-	/* increment the last time to cath the last int */
-	i++;
+	i++;	/* increment the last time to cath the last int */
 
-	/* print without separator */
-	printf("%d", va_arg(args, int));
-
-	/* add new line */
-	printf("\n");
+	printf("%d", va_arg(args, int));	/* print without separator */
+	
+	printf("\n");	/* add new line */
 
 	va_end(args); /* Ends the use of the list */
 
