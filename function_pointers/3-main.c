64c24ab7ab1f87c029f9a1a9f12ae298ee7d928c
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-11-20 12:56:05 +0100

    add error case if more than one operator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -26,6 +26,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][0] == '\0' || argv[2][1] != '\0')
+	{	/* Check if argv[2] is empty or contains more than one character */
+		printf("Error\n");
+		exit(99);
+	}
+
 	if (get_op_func(argv[2]) == NULL)
 	{	/* Check if the function pointer returned by get_op_func is NULL */
 		printf("Error\n");
