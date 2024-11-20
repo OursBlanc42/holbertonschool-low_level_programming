a804cd806296024ef5e89bc1752873ba4c943bf4
OursBlanc42 <simon.regnier@holbertonstudents.com>
2024-11-20 12:50:32 +0100

    update 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,23 +25,22 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	else
-	{
-		if (get_op_func(argv[2]) == NULL)
-		{	/* Check if the function pointer returned by get_op_func is NULL */
-			printf("Error\n");
-			exit(99);
-		}
-		else
-		{	/* Convert input arguments to integers using atoi */
-			num1 = atoi(argv[1]);
-			num2 = atoi(argv[3]);
 
-			/* Call get_op_func to retrieve the appropriate function pointer */
-			/* Immediately invoke the returned function with num1 and num2 */
-			result = get_op_func(argv[2])(num1, num2);
-			printf("%d\n", result);	/* Print the result of the operation */
-		}
+	if (get_op_func(argv[2]) == NULL)
+	{	/* Check if the function pointer returned by get_op_func is NULL */
+		printf("Error\n");
+		exit(99);
 	}
+	else
+	{	/* Convert input arguments to integers using atoi */
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+
+		/* Call get_op_func to retrieve the appropriate function pointer */
+		/* Immediately invoke the returned function with num1 and num2 */
+		result = get_op_func(argv[2])(num1, num2);
+		printf("%d\n", result);	/* Print the result of the operation */
+	}
+
 	return (0);
 }
