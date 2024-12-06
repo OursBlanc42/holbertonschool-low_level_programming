0f5c14ccecf48ca2022dbb174318edbdfab8e76b
OursBlanc <regniersimon@hotmail.com>
2024-12-06 09:38:18 +0100

    update error code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -48,13 +48,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* try to open the file (if the file can not be opened, return 0)*/
+	/* try to open the file (if the file can not be opened, return -1)*/
 	/* the open function will create the file if file doesnt exist */
 	/* O_TRUNC will wipe the data if the file exist  */
 	file_desc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (file_desc == -1)
-		return (0);
+		return (-1);
 
 	/* If text_content = NULL ; the file stay empty */
 	if (text_content == NULL)
