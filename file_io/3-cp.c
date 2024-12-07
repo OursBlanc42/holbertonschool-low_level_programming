227e3f88c7a3a16582df4f00b89af3fc201a10ac
OursBlanc <regniersimon@hotmail.com>
2024-12-07 11:10:24 +0100

    centralize error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,32 +3,51 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void close_properly(int fd_from, int fd_to, char *text_buffer)
+void close_properly(int fd_from, int fd_to, char *text_buffer, ssize_t nb_byte_read, ssize_t nb_print_char)
 {
-	/* Try to close the second file descriptor */
+	/*  Check reading errors */
+	if (nb_byte_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+	}
+
+	/* Check writing error */
+	if (nb_print_char == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+	}
+
+	/* Try to close destination file */
 	if (fd_to >= 0 && close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-
-		/* Free the allocated buffer */
 		if (text_buffer != NULL)
 			free(text_buffer);
-
 		exit(100);
 	}
 
-	/* Try to close the first file descriptor */
+	/* Try to close source file*/
 	if (fd_from >= 0 && close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-
-		/* Free the allocated buffer */
 		if (text_buffer != NULL)
 			free(text_buffer);
-
 		exit(100);
 	}
+
+	/* Free memory if allocated */
+	if (text_buffer != NULL)
+	{
+		free(text_buffer);
+	}
+
+	/* Error output according to error founded */
+	if (nb_byte_read == -1)
+		exit(98); /* Erreur de lecture */
+	if (nb_print_char == -1)
+		exit(99); /* Erreur d'écriture */
 }
+
 
 /**
  * main - copies the content of a file to another file
@@ -76,9 +95,7 @@ int main(int argc, char **argv)
 
 	if ((file_desc_to == -1) || (text_buffer == NULL))
 	{
-		close_properly(file_desc_from, file_desc_to, text_buffer);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
+		close_properly(file_desc_from, file_desc_to, text_buffer, nb_byte_read, nb_print_char);
 	}
 
 /**
@@ -96,9 +113,7 @@ int main(int argc, char **argv)
 		/* error check */
 		if (nb_byte_read == -1)
 		{
-			close_properly(file_desc_from, file_desc_to, text_buffer);
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
+			close_properly(file_desc_from, file_desc_to, text_buffer, nb_byte_read, nb_print_char);
 		}
 
 		if (nb_byte_read > 0) /* avoid writing nothing */
@@ -108,18 +123,13 @@ int main(int argc, char **argv)
 			/* error check */
 			if ((nb_print_char == -1) || (nb_print_char != nb_byte_read))
 			{
-				close_properly(file_desc_from, file_desc_to, text_buffer);
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-				exit(99);
+				close_properly(file_desc_from, file_desc_to, text_buffer, nb_byte_read, nb_print_char);
 			}
 		}
 	}
 
 	/* close each document */
-	close_properly(file_desc_from, file_desc_to, text_buffer);
-
-	/* Free the allocated buffer */
-	free(text_buffer);
+	close_properly(file_desc_from, file_desc_to, text_buffer, nb_byte_read, nb_print_char);
 
 	return (0);
 }
